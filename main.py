cd277e1758a40e4bacc43e2fcd83e816bcf35c98
noplacetogo <51118222+noplacetogo@users.noreply.github.com>
2022-10-31 10:44:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI
-import pymysql
-import asyncio
-import aiomysql
 import mysql as sql
+
 app = FastAPI()
 
 
 
 @app.on_event("startup")
 async  def _startup():
-   app.state.pool  = await sql.SQL()
+   app.state.pool  = await sql.SQL.connect()
 
+@app.get("/getData")
+async  def getData():
+   return await sql.SQL.(app.state.pool,'product')
