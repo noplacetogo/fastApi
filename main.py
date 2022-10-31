8b9152d866c9b75cbf2889cf2da8b0aef1cf49e6
noplacetogo <51118222+noplacetogo@users.noreply.github.com>
2022-10-31 10:40:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,58 +2,12 @@ from fastapi import FastAPI
 import pymysql
 import asyncio
 import aiomysql
+import mysql as sql
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-db_settings = {
-            "host": "192.46.224.179",
-            "port": 3306,
-            "user": "root",
-            "password": "ikok1987",
-            "db": 'shop',
-            "charset": "utf8"
-}
-conn = pymysql.connect(**db_settings)
-
-@app.get('/sqlSearch')
-def sqlSearch():
-    try:       # 建立連線
-        conn.ping()
-        with conn.cursor() as cursor:
-                command = 'SELECT * FROM product;'
-                cursor.execute(command)
-                result = cursor.fetchall()
-                cursor.close()
-                
-                return  result
-    except Exception as ex:
-      # 錯誤回報
-        print(ex)
 
 @app.on_event("startup")
 async  def _startup():
-    db_settings = {
-        "host": "192.46.224.179",
-        "port": 3306,
-        "user": "root",
-        "password": "ikok1987",
-        "db": 'shop',
-        "charset": "utf8",
-    }
-    app.state.pool = await aiomysql.create_pool(**db_settings)
-    print('start done')
-async def test_example(pool):
-    async with pool.acquire() as conn:
-        async with conn.cursor() as cur:
-            await cur.execute("SELECT * FROM product;")
-            return await cur.fetchall()
+   app.state.pool  = await sql.SQL()
 
-
-
-@app.get('/aioSqlSearch')
-async def aioSqlSearch():
-    return  await test_example(app.state.pool)
