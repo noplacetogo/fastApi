8908e8c880a7d61ea8ed3465746186f9000f9aae
noplacetogo <51118222+noplacetogo@users.noreply.github.com>
2022-11-22 07:27:35 +0800

    Update websocket.py

--- routes/websocket.py
+++ routes/websocket.py
@@ -1,13 +1,12 @@
 from typing import List
 
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
-import modules.mysql  as sql
-import modules.other as tool
-import json
 
-app = FastAPI()
+import json
+router = APIRouter()
+
 origins = [
     "*",
 ]
@@ -64,56 +63,9 @@ html = """
 </html>
 """
 
-class chat:
-#   get chat_group where user_id
-    @classmethod
-    def getChatGroups(cls, user_id, target_id=None):
-#       if target_id == None search public
-        if target_id == None:
-            res = sql.query.searchQuery('chat_group',{
-                'user_id': '={}'.format(user_id),
-                'group_status': '=0'
-            })
-        else:
-            query =  "select * from chat_group \
-                        where group_status='1' \
-                        and user_id='{}' \
-                        and group_id = (select group_id from chat_group where user_id ='{}') limit 1;".format(user_id, target_id)
-            res = sql.query.query('chat_group', query)
-        return res
-
-    @classmethod
-    def createChatGroups(cls, group_id, user_id):
-            sql.query.update('chat_group', {
-                'group_token': tool.hash.randomToken(),
-                'group_id': group_id,
-                'user_id': user_id
-            })
-
-    @classmethod
-    def getChatGroupMember(cls, group_id):
-        res = sql.query.searchQuery('chat_group',{
-            'group_token': '={}'.format(group_token)
-        })
-        return res
-
-    @classmethod
-    async def getChatMessage(cls, group_id):
-        res = sql.query.searchQuery('chat_message',{
-            'group_id': '={}'.format(group_id)
-        })
-        return res
-
-    @classmethod
-    async def addChatMessage(cls, group_id, user_id, chat_content):
-        sql.query.update('chat_message',{
-            'chat_token': tool.hash.randomToken(),
-            'group_id': group_id,
-            'user_id': user_id,
-            'chat_content': chat_content
-        })
 
 class ConnectionManager:
+    
     def __init__(self):
         self.active_connections: List[(WebSocket, str)] = []
 
@@ -122,8 +74,8 @@ class ConnectionManager:
         self.active_connections.append((websocket, user_id))
 #       when connect to websocket
 #       get old chat message
-        res = await chat.getChatMessage(group_id)
-        await websocket.send_json(json.dumps({'data': res}, default=str))
+#         res = await chat.getChatMessage(group_id)
+        await websocket.send_json(json.dumps({"data": "大家好~歡迎加入本聊天室"}, default=str))
 
     def disconnect(self, websocket: WebSocket, user_id: str):
         self.active_connections.remove((websocket, user_id))
@@ -161,10 +113,6 @@ async def websocket_endpoint(websocket: WebSocket, user_id: str, group_id: str):
     try:
         while True:
             data = await websocket.receive_json()
-#           record to mysql
-            await chat.addChatMessage(group_id, user_id, data['data'])
-#           send bordcast to custom
             await manager.send_private_message(f"客戶 #{user_id} says: {data['data']}", data['targetID'])
     except WebSocketDisconnect:
         manager.disconnect(websocket, user_id)
-#         await manager.broadcast(f"Client #{client_id} left the chat")
